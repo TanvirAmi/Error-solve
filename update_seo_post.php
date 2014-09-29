4cec272b62971670e2bcf794b757df23000a3cbf
MD. Nahidul Islam Tanvir <tanvir.focus@gmail.com>
2014-09-29 19:13:49 +0600

    Create update_seo_post.php
    This code processes the modified date for SEO.

---
+++ update_seo_post.php
@@ -0,0 +1,21 @@
+<?php
+
+function modified_seo_date(){
+    $u_time = get_the_time('U');
+    $u_modified_time = get_the_modified_time('U');
+if ($u_modified_time > $u_time) {
+    echo "Last updated: <time datetime=";
+    the_modified_time('c');
+    echo " itemprop='dateModified' class='entry-time'>";
+    the_modified_time('F jS, Y');
+    echo "</time>"; }
+else {
+    echo "Published on: <time datetime=";
+    the_time('c');
+    echo " itemprop='datePublished' class='entry-time'>";
+    the_time('F jS, Y');
+    echo "</time>"; 
+    }
+}
+
+?>
