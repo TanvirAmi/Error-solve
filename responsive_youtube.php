04119466953926f94a69086fab16807c1afdfaa0
MD. Nahidul Islam Tanvir <tanvir.focus@gmail.com>
2014-09-29 19:29:19 +0600

    Create responsive_youtube.php

---
+++ responsive_youtube.php
@@ -0,0 +1,21 @@
+==CSS==
+.video-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px; height: 0; overflow: hidden;
+}
+ 
+.video-container iframe,
+.video-container object,
+.video-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+==HTML==
+<div class="video-container">
+         <iframe src="http://www.youtube.com/embed/dFVxGRekRSg" frameborder="0" width="560" height="315"></iframe>
+</div>
